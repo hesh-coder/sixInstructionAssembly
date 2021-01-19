e6e5a6a6505454cd128da349496ff53c5c4455ef
Luca <luca.saleh.2018@live.rhul.ac.uk>
2021-01-19 01:53:16 +0000

    Wrote add function, rest of the maths functions follow similarly.

--- simulator.java
+++ simulator.java
@@ -43,6 +43,7 @@ public class simulator {
 		String currentSet;
 		String[] currentInstructions;
 		String operator;
+		String dst, k, src1, src2;
 
 		for (String current : memLocations) {
 			currentSet = memory.get(current);
@@ -51,19 +52,35 @@ public class simulator {
 
 			switch (operator) {
 				case "1": //set
-					String dst = currentInstructions[1].trim();
-					String k = currentInstructions[2].trim();
+					dst = currentInstructions[1].trim();
+					k = currentInstructions[2].trim();
 					setMemory(dst, k);
 					break;
 				case "2": //beq
 					break;
 				case "3": //add
+					dst = currentInstructions[1].trim();
+					src1 = currentInstructions[2].trim();
+					src2 = currentInstructions[1].trim();
+					addOperator(dst, src1, src2);
 					break;
 				case "4": //sub
+					dst = currentInstructions[1].trim();
+					src1 = currentInstructions[2].trim();
+					src2 = currentInstructions[1].trim();
+					subOperator(dst, src1, src2);
 					break;
 				case "5": //mul
+					dst = currentInstructions[1].trim();
+					src1 = currentInstructions[2].trim();
+					src2 = currentInstructions[1].trim();
+					mulOperator(dst, src1, src2);
 					break;
 				case "6": // div
+					dst = currentInstructions[1].trim();
+					src1 = currentInstructions[2].trim();
+					src2 = currentInstructions[1].trim();
+					divOperator(dst, src1, src2);
 					break;
 			}
 		}
@@ -71,5 +88,33 @@ public class simulator {
 
 	public static void setMemory(String dst, String k) {
 			memory.put(dst, k);
+	}
+
+	public static void addOperator(String dst, String src1, String src2) {
+		String val1 = memory.get(src1);
+		String val2 = memory.get(src2);
+		String prod = String.valueOf((Integer.parseInt(val1)) + (Integer.parseInt(val2)));
+		memory.put(dst, prod);
+	}
+
+	public static void subOperator(String dst, String src1, String src2) {
+		String val1 = memory.get(src1);
+		String val2 = memory.get(src2);
+		String prod = String.valueOf((Integer.parseInt(val1)) - (Integer.parseInt(val2)));
+		memory.put(dst, prod);
+	}
+
+	public static void mulOperator(String dst, String src1, String src2) {
+		String val1 = memory.get(src1);
+		String val2 = memory.get(src2);
+		String prod = String.valueOf((Integer.parseInt(val1)) * (Integer.parseInt(val2)));
+		memory.put(dst, prod);
+	}
+
+	public static void divOperator(String dst, String src1, String src2) {
+		String val1 = memory.get(src1);
+		String val2 = memory.get(src2);
+		String prod = String.valueOf((Integer.parseInt(val1)) / (Integer.parseInt(val2)));
+		memory.put(dst, prod);
 	}
 }
